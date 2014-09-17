6c882a1aeaabeb60416415287eedb94e3da2f9f8
Zeno Zeng <zenoofzeng@gmail.com>
2014-09-17 21:31:54 +0800

    DPI: 300

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
-inkscape -f solar-charging-circuit.svg -e solar-charging-circuit.png
-inkscape -f dc-amplification.svg -e dc-amplification.png
+inkscape -f solar-charging-circuit.svg -d 300 -e solar-charging-circuit.png
+inkscape -f dc-amplification.svg -d 300 -e dc-amplification.png
